cd2c0bb6a5039d523c5f269099c15bf27e07cc9f
jayeshjakkani <61921635+jayeshjakkani@users.noreply.github.com>
2020-09-21 19:00:49 -0400

    Update item_testcases.py

--- test/item_testcases.py
+++ test/item_testcases.py
@@ -18,8 +18,8 @@ flag = 0
 for i in range(len(input_arr)):
     os.system("python3 Code/item_based.py %s > item_test_output%s.txt" %(input_arr[i],i))
     os.system("cp ../SE21-project/test/item_original_output%s.txt item_original_output%s.txt" %(i,i))
-    #os.system("cat item_original_output%s.txt" %(i))
-    #os.system("cat item_test_output%s.txt" %(i))
+    os.system("cat item_original_output%s.txt" %(i))
+    os.system("cat item_test_output%s.txt" %(i))
     file1="item_original_output"+str(i)+".txt"
     file2="item_test_output"+str(i)+".txt"
     comp=check_diff(file1,file2)
