32b880a83c3a77ef56389ca4464aca0e77484d4f
jayeshjakkani <61921635+jayeshjakkani@users.noreply.github.com>
2020-09-21 18:59:01 -0400

    Update item_testcases.py

--- test/item_testcases.py
+++ test/item_testcases.py
@@ -4,7 +4,7 @@ import filecmp
 def check_diff(file1,file2):
     check = {}
     for file in [file1,file2]:
-        with open(file,'r', encoding="utf-8") as f:
+        with open(file,'r',  encoding = "ISO-8859-1") as f:
             check[file] = []
             for line in f:
                 check[file].append(line)
@@ -18,8 +18,8 @@ flag = 0
 for i in range(len(input_arr)):
     os.system("python3 Code/item_based.py %s > item_test_output%s.txt" %(input_arr[i],i))
     os.system("cp ../SE21-project/test/item_original_output%s.txt item_original_output%s.txt" %(i,i))
-    os.system("cat item_original_output%s.txt" %(i))
-    os.system("cat item_test_output%s.txt" %(i))
+    #os.system("cat item_original_output%s.txt" %(i))
+    #os.system("cat item_test_output%s.txt" %(i))
     file1="item_original_output"+str(i)+".txt"
     file2="item_test_output"+str(i)+".txt"
     comp=check_diff(file1,file2)
