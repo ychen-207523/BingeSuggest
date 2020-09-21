672d1f9e56574adc11e5825350b820ac2d8c4fba
jayeshjakkani <61921635+jayeshjakkani@users.noreply.github.com>
2020-09-21 19:14:28 -0400

    Update item_testcases.py

--- test/item_testcases.py
+++ test/item_testcases.py
@@ -16,8 +16,8 @@ def check_diff(file1,file2):
 input_arr = ["34","547","650","14 12"]
 flag = 0
 for i in range(len(input_arr)):
-    os.system("python3 Code/item_based.py %s > item_test_output%s.txt" %(input_arr[i],i))
-    os.system("cp ../SE21-project/test/item_original_output%s.txt item_original_output%s.txt" %(i,i))
+    os.system("python3 Code/item_based.py %s > item_original_output%s.txt" %(input_arr[i],i))
+    os.system("python3 Code/item_based_test.py %s > item_test_output%s.txt" %(input_arr[i],i))
     os.system("cat item_original_output%s.txt" %(i))
     os.system("cat item_test_output%s.txt" %(i))
     file1="item_original_output"+str(i)+".txt"
