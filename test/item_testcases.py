c9393a2afe0580ba833a9b0b7551b82f63e9d8b7
jayeshjakkani <61921635+jayeshjakkani@users.noreply.github.com>
2020-09-21 18:52:26 -0400

    Update item_testcases.py

--- test/item_testcases.py
+++ test/item_testcases.py
@@ -1,6 +1,18 @@
 import os
 import filecmp
 
+def check_diff(file1,file2):
+    check = {}
+    for file in [file1,file2]:
+        with open(file,'r') as f:
+            check[file] = []
+            for line in f:
+                check[file].append(line)
+    diff = set(check[file1]) - set(check[file2])
+    if diff:
+        return(0)
+    return(1)
+
 input_arr = ["34","547","650","14 12"]
 flag = 0
 for i in range(len(input_arr)):
@@ -10,7 +22,8 @@ for i in range(len(input_arr)):
     os.system("cat item_test_output%s.txt" %(i))
     file1="item_original_output"+str(i)+".txt"
     file2="item_test_output"+str(i)+".txt"
-    comp = filecmp.cmp(file1, file2)
+    comp=check_diff(file1,file2)
+    
     if comp:
         print("Test case "+str(i+1)+" passed")
         flag+=1
