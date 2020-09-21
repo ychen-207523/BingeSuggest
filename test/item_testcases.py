f3b4ca6058f78d14357a400d7d51cbd6c9d75a1a
jayeshjakkani <61921635+jayeshjakkani@users.noreply.github.com>
2020-09-21 18:15:52 -0400

    Update item_testcases.py

--- test/item_testcases.py
+++ test/item_testcases.py
@@ -4,9 +4,9 @@ import filecmp
 input_arr = ["34","547","650","14 12"]
 flag = 0
 for i in range(len(input_arr)):
-    os.system("python3 II.py %s > item_test_output%s.txt" %(input_arr[i],i))
-    file1="C:\\Users\\jayes\\draft1\\item_original_output"+str(i)+".txt"
-    file2="C:\\Users\\jayes\\draft1\\item_test_output"+str(i)+".txt"
+    os.system("python3 item_based.py %s > item_test_output%s.txt" %(input_arr[i],i))
+    file1="item_original_output"+str(i)+".txt"
+    file2="item_test_output"+str(i)+".txt"
     comp = filecmp.cmp(file1, file2)
     if comp:
         print("Test case "+str(i+1)+" passed")
