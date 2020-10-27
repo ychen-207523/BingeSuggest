d4eb95edfcc9c87a568c7b78479036f5a0c1ce2a
patel97 <patelamit791@gmail.com>
2020-10-26 23:27:43 -0400

    Final work completed

--- Code/text based approach/plot_based_recommendation.py
+++ Code/text based approach/plot_based_recommendation.py
@@ -6,22 +6,22 @@ from sklearn.metrics.pairwise import linear_kernel
 
 
 def get_data(movie_length):
-    metadata = pd.read_csv('movies_metadata.csv', low_memory=False)
+    metadata = pd.read_csv("movies_metadata.csv", low_memory=False)
     return metadata[:movie_length]
 
+
 def compute_tfidfmatrix(metadata):
-    #Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
-    tfidf = TfidfVectorizer(stop_words='english')
+    # Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+    tfidf = TfidfVectorizer(stop_words="english")
 
-    #Replace NaN with an empty string
-    metadata['overview'] = metadata['overview'].fillna('')
+    # Replace NaN with an empty string
+    metadata["overview"] = metadata["overview"].fillna("")
 
-    #Construct the required TF-IDF matrix by fitting and transforming the data
-    tfidf_matrix = tfidf.fit_transform(metadata['overview'])
+    # Construct the required TF-IDF matrix by fitting and transforming the data
+    tfidf_matrix = tfidf.fit_transform(metadata["overview"])
 
     cosine_similarity = linear_kernel(tfidf_matrix, tfidf_matrix)
-    np.savez('cosine_similarity_10k', matrix=cosine_similarity) 
-
+    np.savez("cosine_similarity_10k", matrix=cosine_similarity)
 
 
 def get_recommendations(title, indices, cosine_sim):
@@ -48,19 +48,19 @@ def get_recommendations(title, indices, cosine_sim):
     movie_indices = [i[0] for i in sim_scores]
 
     # Return the top 10 most similar movies
-    return metadata['title'].iloc[movie_indices]
+    return metadata["title"].iloc[movie_indices]
 
 
 if __name__ == "__main__":
     metadata = get_data(movie_length=5000)
-    
+
     # compute_tfidfmatrix(metadata)
 
     # Cosine similarity matrix is already saved.
-    data = np.load('cosine_similarity_5k.npz', allow_pickle=True) 
-    cosine_similarity = data['matrix']
+    data = np.load("cosine_similarity_5k.npz", allow_pickle=True)
+    cosine_similarity = data["matrix"]
 
-    indices = pd.Series(metadata.index, index=metadata['title']).drop_duplicates()
+    indices = pd.Series(metadata.index, index=metadata["title"]).drop_duplicates()
     play = True
     while play != False:
         movie = input("Name of movie from movie list: ")
@@ -68,8 +68,8 @@ if __name__ == "__main__":
         if recommendations is None:
             print("Given movie not in database, try again")
             continue
-        
-        print("Following are the recommended movies if you like : ", movie )
+
+        print("Following are the recommended movies if you like : ", movie)
         print(recommendations)
         response = input("Do you want to continue?(yes/no)")
         if response == "no":
