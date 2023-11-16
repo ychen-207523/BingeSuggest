77309bff0ca31590c7341f90d1c5acde6af4609e
bswalia <bswalia@ncsu.edu>
2023-11-16 15:49:30 -0500

    Added part of yml file to run the init file for the db

--- test/test_util.py
+++ test/test_util.py
@@ -81,7 +81,6 @@ class Tests(unittest.TestCase):
         """
         db = mysql.connector.connect(user='root', password='testrootpass',
                                 host='127.0.0.1')
-        self.setUpMockDB(db)
         createAccount(db, "test@test.com", "testUser", "testPassword")
         expectedUserName="testUser"
         expectedEmail = "test@test.com"
@@ -100,54 +99,54 @@ class Tests(unittest.TestCase):
         db.close()
     
 
-    def setUpMockDB(self, db):
-        exector = db.cursor()
-        exector.execute("DROP DATABASE IF EXISTS testDB1;")
-        exector.execute("CREATE DATABASE testDB1;")
-        exector.execute("USE testDB1;")
-        exector.execute("""
-                        CREATE TABLE IF NOT EXISTS Users (
-                        idUsers INT NOT NULL AUTO_INCREMENT,
-                        username VARCHAR(45) NOT NULL,
-                        email VARCHAR(45) NOT NULL,
-                        password VARCHAR(45) NOT NULL,
-                        PRIMARY KEY (idUsers),
-                        UNIQUE INDEX username_UNIQUE (username ASC),
-                        UNIQUE INDEX email_UNIQUE (email ASC)
-                        ) ENGINE = InnoDB;
-                        """)
-        exector.execute("""
-                        CREATE TABLE IF NOT EXISTS Movies (
-                        idMovies INT NOT NULL AUTO_INCREMENT,
-                        name VARCHAR(45) NOT NULL,
-                        imdb_id VARCHAR(45) NOT NULL,
-                        PRIMARY KEY (idMovies),
-                        UNIQUE INDEX imdb_id_UNIQUE (imdb_id ASC)
-                        ) ENGINE = InnoDB;
-                        """)
-        exector.execute("""
-                        CREATE TABLE IF NOT EXISTS Ratings (
-                        idRatings INT NOT NULL AUTO_INCREMENT,
-                        user_id INT NOT NULL,
-                        movie_id INT NOT NULL,
-                        score INT NOT NULL,
-                        review VARCHAR(45) NULL,
-                        time DATETIME NOT NULL,
-                        PRIMARY KEY (idRatings),
-                        INDEX user_id_idx (user_id ASC),
-                        INDEX movie_id_idx (movie_id ASC),
-                        CONSTRAINT user_id
-                        FOREIGN KEY (user_id)
-                        REFERENCES Users (idUsers)
-                        ON DELETE NO ACTION
-                        ON UPDATE NO ACTION,
-                        CONSTRAINT movie_id
-                        FOREIGN KEY (movie_id)
-                        REFERENCES Movies (idMovies)
-                        ON DELETE NO ACTION
-                        ON UPDATE NO ACTION
-                        ) ENGINE = InnoDB;
-                        """)
+    # def setUpMockDB(self, db):
+    #     exector = db.cursor()
+    #     exector.execute("DROP DATABASE IF EXISTS testDB1;")
+    #     exector.execute("CREATE DATABASE testDB1;")
+    #     exector.execute("USE testDB1;")
+    #     exector.execute("""
+    #                     CREATE TABLE IF NOT EXISTS Users (
+    #                     idUsers INT NOT NULL AUTO_INCREMENT,
+    #                     username VARCHAR(45) NOT NULL,
+    #                     email VARCHAR(45) NOT NULL,
+    #                     password VARCHAR(45) NOT NULL,
+    #                     PRIMARY KEY (idUsers),
+    #                     UNIQUE INDEX username_UNIQUE (username ASC),
+    #                     UNIQUE INDEX email_UNIQUE (email ASC)
+    #                     ) ENGINE = InnoDB;
+    #                     """)
+    #     exector.execute("""
+    #                     CREATE TABLE IF NOT EXISTS Movies (
+    #                     idMovies INT NOT NULL AUTO_INCREMENT,
+    #                     name VARCHAR(45) NOT NULL,
+    #                     imdb_id VARCHAR(45) NOT NULL,
+    #                     PRIMARY KEY (idMovies),
+    #                     UNIQUE INDEX imdb_id_UNIQUE (imdb_id ASC)
+    #                     ) ENGINE = InnoDB;
+    #                     """)
+    #     exector.execute("""
+    #                     CREATE TABLE IF NOT EXISTS Ratings (
+    #                     idRatings INT NOT NULL AUTO_INCREMENT,
+    #                     user_id INT NOT NULL,
+    #                     movie_id INT NOT NULL,
+    #                     score INT NOT NULL,
+    #                     review VARCHAR(45) NULL,
+    #                     time DATETIME NOT NULL,
+    #                     PRIMARY KEY (idRatings),
+    #                     INDEX user_id_idx (user_id ASC),
+    #                     INDEX movie_id_idx (movie_id ASC),
+    #                     CONSTRAINT user_id
+    #                     FOREIGN KEY (user_id)
+    #                     REFERENCES Users (idUsers)
+    #                     ON DELETE NO ACTION
+    #                     ON UPDATE NO ACTION,
+    #                     CONSTRAINT movie_id
+    #                     FOREIGN KEY (movie_id)
+    #                     REFERENCES Movies (idMovies)
+    #                     ON DELETE NO ACTION
+    #                     ON UPDATE NO ACTION
+    #                     ) ENGINE = InnoDB;
+    #                     """)
 
 
 
