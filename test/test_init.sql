674c9e5e4c0768e83196a64229df76203eff59ca
rpkenney <kenneyrobert109@gmail.com>
2023-11-27 15:04:20 -0500

    fix sql comments

--- test/test_init.sql
+++ test/test_init.sql
@@ -1,7 +1,7 @@
---Copyright (c) 2023 Nathan Kohen, Nicholas Foster, Brandon Walia, Robert Kenney
---This code is licensed under MIT license (see LICENSE for details)
+-- Copyright (c) 2023 Nathan Kohen, Nicholas Foster, Brandon Walia, Robert Kenney
+-- This code is licensed under MIT license (see LICENSE for details)
 
---@author: PopcornPicks
+-- @author: PopcornPicks
 
 CREATE DATABASE IF NOT EXISTS testDB;
 
