e6904fecd3c31eeaec4a73a76e98cee5dc5489c4
Devanshu Shah <dastaryt720@gmail.com>
2024-10-28 13:48:33 -0400

    Features Addition in Data

--- src/recommenderapp/data.py
+++ src/recommenderapp/data.py
@@ -7,7 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 ia = IMDb()
 i = 0
 
-def get_imdb_rating_and_awards(imdb_id):
+def get_imdb_rating(imdb_id):
     global i
     i += 1
     print(i)
@@ -17,15 +17,12 @@ def get_imdb_rating_and_awards(imdb_id):
     try:
         movie = ia.get_movie(imdb_id[2:])  # Removing 'tt' prefix
         rating = movie.get('rating', "No Rating Found")
-        
-        # Fetching awards if available
-        awards = movie.get('awards', "No Awards Found")
-        return rating, awards
+        return rating
     except Exception as e:
         print(f"Error fetching data for IMDb ID {imdb_id}: {e}")
         return "Error", "Error"
 
-def update_csv_with_rating_and_awards(file_path):
+def update_csv_with_rating(file_path):
     # Load the CSV file
     df = pd.read_csv(file_path)
 
@@ -34,28 +31,25 @@ def update_csv_with_rating_and_awards(file_path):
         print("Error: 'imdb_id' column not found in the CSV file.")
         return
 
-    # Check if 'rating' and 'awards' columns exist; if not, add them
+    # Check if 'rating' column exist; if not, add it
     if 'rating' not in df.columns:
         df['rating'] = None
-    if 'awards' not in df.columns:
-        df['awards'] = None
 
     # Filter rows where rating or awards are still missing
-    imdb_ids_to_fetch = df[(df['rating'].isnull()) | (df['awards'].isnull())]['imdb_id'].tolist()
+    imdb_ids_to_fetch = df[df['rating'].isnull()]['imdb_id'].tolist()
 
     # Fetch rating and awards details concurrently for missing entries
     with ThreadPoolExecutor(max_workers=4) as executor:
-        results = list(executor.map(get_imdb_rating_and_awards, imdb_ids_to_fetch))
+        results = list(executor.map(get_imdb_rating, imdb_ids_to_fetch))
 
-    # Split the results into rating and awards columns
+    # Split the results into rating column
     ratings, awards = zip(*results)
     df.loc[df['rating'].isnull(), 'rating'] = ratings
-    df.loc[df['awards'].isnull(), 'awards'] = awards
 
     # Save the updated DataFrame back to the CSV
     df.to_csv(file_path, index=False)
-    print(f"Updated CSV saved with IMDb ratings and awards in '{file_path}'.")
+    print(f"Updated CSV saved with IMDb ratings in '{file_path}'.")
 
 # Usage example
 csv_file_path = r'.\data\movies.csv'  # Replace with your file path
-update_csv_with_rating_and_awards(csv_file_path)
+update_csv_with_rating(csv_file_path)
