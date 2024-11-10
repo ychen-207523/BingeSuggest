ebc38bf3e48bb140cb99ff939ecd25cc99dd6b7d
GitHub Action <action@github.com>
2024-11-10 20:26:51 +0000

    Automatically format code with Black

--- src/recommenderapp/app.py
+++ src/recommenderapp/app.py
@@ -410,7 +410,7 @@ def before_request():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
     )
 
 
