8cbae3602fc5d7e6317842f1582a61dfdfcdc803
GitHub Action <action@github.com>
2024-11-23 19:40:57 +0000

    Automatically format code with Black

--- test_v7/watchedHistory/test_watched_history.py
+++ test_v7/watchedHistory/test_watched_history.py
@@ -15,6 +15,7 @@ sys.path.append(str(Path(__file__).resolve().parents[2]))
 from src.recommenderapp.app import app
 from src.recommenderapp.utils import create_account
 
+
 class TestWatchedHistory(unittest.TestCase):
     """
     Test cases for Watched History functionality.
